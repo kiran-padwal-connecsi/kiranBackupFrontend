af1b77fff4ba98818a5c2d119bd228e9c0ba8a3d
kiranPadwal <padwalkiran1985@gmail.com>
2019-03-12 12:34:14 +0100

    bug edit profile inf

--- connecsiApp.py
+++ connecsiApp.py
@@ -2426,7 +2426,7 @@ from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '0'
 # os.environ['WERKZEUG_RUN_MAIN'] = 'true'
 
 
